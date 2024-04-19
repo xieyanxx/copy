ececd951340dd7d5db198e38263433eaf725d0f1
xieyan <1107582086@qq.com>
2024-04-19 14:25:15 +0800

    fix: 调试接口

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,18 @@ import { useCallback, useEffect, useState } from "react";
 import styles from "./index.less";
 
 export default function HomePage() {
-  const [text, setText] = useState<string>("121212121");
+  const [text, setText] = useState<string>("");
+  // const [text1, setText1] = useState<string>("");
   const getData = useCallback(() => {
     getText().then((res: any) => {
       let copyText = res.data.filter(
         (item: any) => item.name == "promotionCopy1"
       )[0].value;
+      // let copyText1 = res.data.filter(
+      //   (item: any) => item.name == "prohibitedItems"
+      // )[0].value;
       setText(copyText);
-      console.log(res);
+      // setText1(copyText1);
     });
   }, []);
   useEffect(() => {
@@ -20,12 +24,30 @@ export default function HomePage() {
   return (
     <div className={styles.wrap}>
       <div className={styles.content}>
+        1)copt this text :(all blue words)
         <p id="text">{text}</p>
         <div
           className={styles.copy_btn}
           onClick={(e) => handleClipboard(e, text)}
         >
           copy
+        </div>
+        <div className={styles.content}>
+          2) go to:
+          <a
+            href="https://twitter.com/compose/post"
+            target="_blank"
+            className={styles.link}
+          >
+            https://twitter.com/compose/post
+          </a>
+          <p className={styles.text}>
+            3) On proof 1 you must provide your correct tweet URL to get paid.
+            <br />
+            <br />
+            ❌Don't create multiple accounts <br /> ❌ Don't use VPN or proxy
+            <br /> ❌ Don't use dot mail or Outlook Mail
+          </p>
         </div>
       </div>
     </div>
